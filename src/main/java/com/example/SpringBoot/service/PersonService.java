4fafe8f80bab008d1678817840df0bc05b9d2511
minh <hoangminh1612@gmail.com>
2020-03-01 17:29:35 -0800

    Update HTTP-GET API

---
+++ src/main/java/com/example/SpringBoot/service/PersonService.java
@@ -0,0 +1,34 @@
+package com.example.SpringBoot.service;
+
+import com.example.SpringBoot.dao.PersonDao;
+import com.example.SpringBoot.model.Person;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+public class PersonService {
+
+    private final PersonDao personDao;
+
+    @Autowired
+    public PersonService(@Qualifier("fakeDao") PersonDao personDao) {
+        this.personDao = personDao;
+    }
+
+    public int addPerson(Person person) {
+        return personDao.addPerson(person);
+    }
+
+    public List<Person> getAllPeople() {
+        return personDao.selectAllPeople();
+    }
+
+    public Optional<Person> getPersonById(UUID id) {
+        return personDao.selectPersonById(id);
+    }
+}
